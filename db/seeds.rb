8a59471fb9b569161d16cd99b1725d191c8f6b5b
KellyRudnicki <kelly.rudnicki@comcast.net>
2019-12-06 13:37:41 -0700

    cleaned up code, added theme song and stylized welcome menu

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,27 +6,6 @@ Player.destroy_all
 FantasyTeam.destroy_all
 
 
-# user1 = User.create(username: "idk")
-
-# league1 = League.create(league_name: "The Big League")
-
-# league_user1 = LeagueUser.create(league_id: league1.id, user_id: user1.id)
-
-
-# player1 = Player.create(name: "Ryan Suter", position: "defenseman", points: 17, goals: 2)
-# player2 = Player.create(name: "Sydney Crosby", position: "forward_1", points: 32, goals: 20)
-# player3 = Player.create(name: "Connor McDavid", position: "forward_2", points: 25, goals: 17)
-# player4 = Player.create(name: "Nathan MacKinnon", position: "goalie", points: 5, goals: 1)
-
-
-# fantasy_team4 = FantasyTeam.create(team_name: "The fourth team", league_id: 15)
-# fantasy_team5 = FantasyTeam.create(team_name: "The fifth team", league_id: 15)
-
-
-# PlayerTeam.create(player_id: player1.id, fantasy_team_id: fantasy_team4.id)
-# PlayerTeam.create(player_id: player2.id, fantasy_team_id: fantasy_team4.id)
-
-
     response = RestClient.get("https://statsapi.web.nhl.com/api/v1/teams?expand=team.roster")
     result = JSON.parse(response)
 
--- lib/app.rb
+++ lib/app.rb
@@ -3,10 +3,14 @@ class App
     attr_reader :user 
 
     def start_game
-        puts "Welcome to Fantasy Hockey! What is your username?"
+        font = TTY::Font.new(:doom)
+        pastel = Pastel.new
+        puts pastel.cyan(font.write("ICE  ICE  BABY !"))
+        font2 = TTY::Font.new(:straight)
+        puts font2.write(" CREATE   A   USERNAME: ")
         user_input = gets.chomp
         @user = User.create(username: user_input)
-        puts "Welcome #{@user.username}!" 
+        puts ("Welcome #{@user.username}!").colorize(:light_cyan)
         main_menu
     end 
     
@@ -25,10 +29,8 @@ class App
                 view_leagues
             elsif user_input == "Skip to Team Menu"
                 team_menu
-            elsif user_input == "Quit"
+            else user_input == "Quit"
                 exit 
-            else
-            puts "Sorry that's not a valid option, please try again!"
             main_menu
         end 
     end 
@@ -53,7 +55,7 @@ class App
         user_input = prompt.select("What league would you like to join?", total_leagues)
         @user.leagues << user_input
 
-        main_menu
+        create_team
     end 
 
     def view_leagues
@@ -65,17 +67,34 @@ class App
         main_menu
     end 
 
+    # def view_leagues
+    #     my_leagues = @user.leagues.map do |league|
+    #         league_scroll = league.league_name
+    #         {league_scroll => league}
+    #     end
+
+    #     prompt = TTY::Prompt.new
+    #     user_input = prompt.select("Currently you belong to:", my_leagues)
+    #     # @my_teams = FantasyTeam.all.select{|team| team.league_id == user_input.id}
+    #     @my_fantasy_team
+
+    #     binding.pry
+
+    #     main_menu
+    # end
+
     def team_menu
         prompt = TTY::Prompt.new
-        user_input = prompt.select("May we suggest...", ["Create a Team", "Manage a Team", "Delete a Team", "Main Menu"]) 
+        user_input = prompt.select("May we suggest...", ["Create a Team", "Manage a Team", "Delete a Team", "View my Team(s)", "Main Menu"]) 
     
             if user_input == "Create a Team"
                 create_team
             elsif user_input == "Manage a Team"
                 manage_team
             elsif user_input == "View my Team(s)"
-                puts "Sorry this option is still in progress" 
-                #delete_team
+                view_teams
+            elsif user_input == "Delete a Team"
+                delete_team
             elsif user_input == "Main Menu"
                 main_menu
             else
@@ -94,36 +113,15 @@ class App
         main_menu
     end 
 
-    # def delete_team 
-
-    #     prompt = TTY::Prompt.new
-
-    #     binding.pry
-
-    #     my_team = @my_fantasy_team.players.map do |player|
-    #         player_scroll = "#{player.name}, #{player.position}   Points: #{player.points}   Goals: #{player.goals}"
-    #         {player_scroll => player}
-    #     end
-
-    #     user_input = prompt.select("What team would you like to delete?", my_team)
-
-    #     @my_fantasy_team.players.delete(user_input)
-
-    #     puts "Time to replace #{user_input.name}!"
-
-    #     add_player
-
-    # end 
-
     def add_roster
 
-        player_stats = Player.all.map do |player|
+        @player_stats = Player.all.map do |player|
             player_scroll = "#{player.name}, #{player.position}   Points: #{player.points}   Goals: #{player.goals}"
             {player_scroll => player}
         end
 
         prompt = TTY::Prompt.new
-        user_input = prompt.multi_select("Please choose 5 players to help you win the cup!", player_stats)
+        user_input = prompt.multi_select("Please choose 5 players to help you win the cup!", @player_stats)
         @my_fantasy_team.players << user_input
     end 
 
@@ -142,6 +140,19 @@ class App
             end 
     end 
 
+    def delete_team
+        puts "Stay tuned, this feature is coming soon!!"
+
+        main_menu
+    end
+
+    def view_teams
+
+        puts "Stay tuned, this feature is coming soon!!"
+
+        main_menu
+    end 
+
     def add_player 
 
         prompt = TTY::Prompt.new
@@ -151,7 +162,7 @@ class App
             {player_scroll => player}
         end
 
-        user_input = prompt.select("What player would like to add?", my_team)
+        user_input = prompt.select("What player would like to add?", @player_stats)
 
         @my_fantasy_team.players << user_input
 
